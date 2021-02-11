3bf999dc72a999ef5552db3e236f8e200d755b55
ehigie aito (Pystar) <aitoehigie@gmail.com>
2021-02-11 03:13:04 +0100

    redacted key

--- pollster/settings.py
+++ pollster/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jao2t5natg@81pfo*4dk@2!slg-fi8zza*@ijwx+3e#3$wu=@b'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
